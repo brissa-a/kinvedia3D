f62046770bedac0c3c571d1fd3f73f5283de1112
Alexis Brissard <alexis.brissard@epitech.eu>
2012-10-14 14:33:28 +0200

    Hello world !

---
+++ Kinvedia3D/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kinvedia3D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 42;
+            Console.WriteLine("Hello World !!" + i);
+        }
+    }
+}
